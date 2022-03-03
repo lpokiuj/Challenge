512f8ea8b3e9b9c29868c075135553e40c8a5b5b
lpokiuj <michael082@binus.ac.id>
2022-03-03 22:24:31 +0700

    cleanup dummy data after testing

--- test/server.spec.js
+++ test/server.spec.js
@@ -7,56 +7,66 @@ chai.should();
 
 chai.use(chaiHttp);
 
+// Take User model
+const User = require('../src/models/user.model');
+// Take Post model
+const Post = require('../src/models/post.model');
+
 describe("Testing API", () => {
-  it("Register - Login - Post - Update Email", (done) => {
-    chai
-      .request(server)
-      .post("/api/auth/register")
-      .send({
-        name: "Michael",
-        email: "michael@gmail.com",
-        password: "janganliat",
-      })
-      .end((err, res) => {
-        res.should.have.status(201);
-        res.body.should.have.property("id");
-        const id = res.body.id;
+    it("Register - Login - Post - Update Email", (done) => {
         chai
-          .request(server)
-          .post("/api/auth/login")
-          .send({
+        .request(server)
+        .post("/api/auth/register")
+        .send({
+            name: "Michael",
             email: "michael@gmail.com",
             password: "janganliat",
-          })
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.have.property("token");
-            const token = res.body.token;
+        })
+        .end((err, res) => {
+            res.should.have.status(201);
+            res.body.should.have.property("id");
+            const id = res.body.id;
             chai
-              .request(server)
-              .post("/api/post/")
-              .set("auth-token", token)
-              .send({
-                title: "Title 1",
-                description: "Description of title 1",
-              })
-              .end((err, res) => {
-                res.should.have.status(201);
-                res.body.should.have.property("post");
+            .request(server)
+            .post("/api/auth/login")
+            .send({
+                email: "michael@gmail.com",
+                password: "janganliat",
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.have.property("token");
+                const token = res.body.token;
                 chai
-                  .request(server)
-                  .put("/api/user/" + id)
-                  .set("auth-token", token)
-                  .send({
-                    email: "michael@yahoo.com",
-                  })
-                  .end((err, res) => {
+                .request(server)
+                .post("/api/post/")
+                .set("auth-token", token)
+                .send({
+                    title: "Title 1",
+                    description: "Description of title 1",
+                })
+                .end((err, res) => {
                     res.should.have.status(201);
-                    res.body.should.have.property("id");
-                    done();
-                  });
-              });
-          });
-      });
-  });
+                    res.body.should.have.property("post");
+                    chai
+                    .request(server)
+                    .put("/api/user/" + id)
+                    .set("auth-token", token)
+                    .send({
+                        email: "michael@yahoo.com",
+                    })
+                    .end((err, res) => {
+                        res.should.have.status(201);
+                        res.body.should.have.property("id");
+                        done();
+                    });
+                });
+            });
+        });
+    });
+    afterEach('Cleanup dummy data', (done) => {
+        User.deleteOne({ name: 'Michael' });
+        Post.deleteOne({ title: 'Title 1' });
+        done();
+    });
 });
